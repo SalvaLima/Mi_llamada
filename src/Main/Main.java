1e8863005f33bdb90ee2e3a3c779f27615a74a47
Salva <salvalima02@gmail.com>
2022-01-14 20:24:57 +0100

    A

---
+++ src/Main/Main.java
@@ -0,0 +1,24 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Double a,b,c;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Inserta el valor de a :");
+        a = scn.nextDouble();
+        System.out.println("Inserta el valor de b :");
+        b = scn.nextDouble();
+        System.out.println("Inserta el valor de c :");
+        c = scn.nextDouble();
+
+        /**
+         * -b (+-) (b^2 - 4 ac) / 2a
+         */
+
+
+    }
+}
